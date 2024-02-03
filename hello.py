820ee9d60ed89db9c2e07ed8f2284927089b0663
viktorchertov <victorchertov@gmail.com>
2024-02-03 13:53:43 +0530

    learn variables and data types

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-print("hello world")
-print("hello i am programmer")
+result = ((2+6)*2)**(1/2)
+print(result)
+
+result = "result: "+str(result)
+print(result)
+
+birth_year = input("enter your birth year: ")
+age = 2024 - int(birth_year)
+print("your age is: "+str(age))
